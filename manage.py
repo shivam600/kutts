9f11b90dafd0f112f8ab2f1128a5d5d7beb9ef58
shivam karan <73827549+shivam600@users.noreply.github.com>
2021-06-10 01:32:23 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Goog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
